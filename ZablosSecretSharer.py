3504ec20bdd6f475b9dc3b3076c3884105b24cfc
Zablo <kintaro1301@aol.com>
2025-09-30 09:10:34 +0200

    Update ZablosSecretSharer.py
    - codes now show treshold (C1/2)
    - replaced split with paste function

--- ZablosSecretSharer.py
+++ ZablosSecretSharer.py
@@ -1,4 +1,4 @@
-### Zablos Secret Sharer V3.0.2 One Time Pad ###
+### Zablos Secret Sharer V3.0.3 One Time Pad ###
 import os
 import sys
 from secrets import randbelow
@@ -61,7 +61,7 @@ def translate_into_mastercode(message=None):
             print(f"[Error] {e}")
             return
     clear()
-    print(f'C1: {c1str}')
+    print(f'C1/{shares}: {c1str}')
     
     for x in message:
         if x == " ":
@@ -99,7 +99,7 @@ def translate_into_mastercode(message=None):
         code2_list = [masterdecode[x % 100] for x in c2]
         codes_global['C2'] = code2_list
         code2_str = ''.join(code2_list)
-        print(f'C2: {code2_str}')
+        print(f'C2/{shares}: {code2_str}')
     else:
         pseudo_random_numbers(messageMaster, UccMaster, shares)
 
@@ -109,7 +109,7 @@ def pseudo_random_numbers(messageMaster, UccMaster, shares):
     random_codes = {}
     count = len(UccMaster)
     for x in range(2, shares):
-        key = f'C{x}'
+        key = f'C{x}/{shares}'
         value = []
         for _ in range(count):
             value.append(randbelow(100))
@@ -120,7 +120,7 @@ def pseudo_random_numbers(messageMaster, UccMaster, shares):
 def last_code(messageMaster, random_codes, UccMaster, shares):
     global codes_global
     for i in range(2, shares):
-        key = f'C{i}'
+        key = f'C{i}/{shares}'
         templist = random_codes[key]
         codes_global[key] = [masterdecode[y] for y in templist]
         messageOut = ''.join(codes_global[key])
@@ -136,7 +136,7 @@ def last_code(messageMaster, random_codes, UccMaster, shares):
     last_code_chars = [masterdecode[x] for x in last_code_list]
     codes_global[f'C{shares}'] = last_code_chars
     messageOut = ''.join(last_code_chars)
-    print(f'C{shares}: {messageOut}')
+    print(f'C{shares}/{shares}: {messageOut}')
 
 
 def decode_message():
@@ -145,7 +145,7 @@ def decode_message():
     copylist = []
     x = 1
     for _ in range(shares):
-        key = f'C{x}'
+        key = f'C{x}/{shares}'
         value = list(input(f'{key}: '))
         try:
             validate_code_input(value)
@@ -216,15 +216,10 @@ def generate_otp():
 def menu():
     while True:
         menu_choice = input(
-            "(S)plit, (P)aste, (C)ombine, (M)astercode, (O)TP, (W)ipe, (Q)uit? "
+            "(S)plit, (C)ombine, (M)astercode, (O)TP, (W)ipe, (Q)uit? "
         ).lower()
         match menu_choice:
             case "s":
-                try:
-                    translate_into_mastercode()
-                except ValueError as e:
-                    print(f"[Error] {e}")
-            case "p":
                 print("Paste your message below. End input with a line containing END+++")
                 lines = []
                 while True:
