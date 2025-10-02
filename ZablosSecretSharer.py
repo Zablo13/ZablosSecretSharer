a98b88bc5964e4218f02cfad4d7b3638281bd481
Zablo <kintaro1301@aol.com>
2025-10-02 11:19:44 +0200

    Update ZablosSecretSharer.py
    output Code replaced -> Share
                                         -> OTP

--- ZablosSecretSharer.py
+++ ZablosSecretSharer.py
@@ -1,4 +1,4 @@
-### Zablos Secret Sharer V3.0.4 One Time Pad ###
+### Zablos Secret Sharer V3.0.5 One Time Pad ###
 import os
 import sys
 from secrets import randbelow
@@ -61,7 +61,7 @@ def translate_into_mastercode(message=None):
             print(f"[Error] {e}")
             return
     clear()
-    print(f'C1/{shares}: {c1str}')
+    print(f'Share1/{shares}: {c1str}')
     
     for x in message:
         if x == " ":
@@ -99,7 +99,7 @@ def translate_into_mastercode(message=None):
         code2_list = [masterdecode[x % 100] for x in c2]
         codes_global['C2'] = code2_list
         code2_str = ''.join(code2_list)
-        print(f'C2/{shares}: {code2_str}')
+        print(f'Share2/{shares}: {code2_str}')
     else:
         pseudo_random_numbers(messageMaster, UccMaster, shares)
 
@@ -109,7 +109,7 @@ def pseudo_random_numbers(messageMaster, UccMaster, shares):
     random_codes = {}
     count = len(UccMaster)
     for x in range(2, shares):
-        key = f'C{x}/{shares}'
+        key = f'Share{x}/{shares}'
         value = []
         for _ in range(count):
             value.append(randbelow(100))
@@ -120,7 +120,7 @@ def pseudo_random_numbers(messageMaster, UccMaster, shares):
 def last_code(messageMaster, random_codes, UccMaster, shares):
     global codes_global
     for i in range(2, shares):
-        key = f'C{i}/{shares}'
+        key = f'Share{i}/{shares}'
         templist = random_codes[key]
         codes_global[key] = [masterdecode[y] for y in templist]
         messageOut = ''.join(codes_global[key])
@@ -134,9 +134,9 @@ def last_code(messageMaster, random_codes, UccMaster, shares):
     copylist = [x % 100 for x in finalSum]
     last_code_list = [((m - c) % 100) for m, c in zip(messageMaster, copylist)]
     last_code_chars = [masterdecode[x] for x in last_code_list]
-    codes_global[f'C{shares}'] = last_code_chars
+    codes_global[f'Share{shares}'] = last_code_chars
     messageOut = ''.join(last_code_chars)
-    print(f'C{shares}/{shares}: {messageOut}')
+    print(f'Share{shares}/{shares}: {messageOut}')
 
 
 def decode_message():
@@ -145,7 +145,7 @@ def decode_message():
     copylist = []
     x = 1
     for _ in range(shares):
-        key = f'C{x}/{shares}'
+        key = f'Shares{x}/{shares}'
         value = list(input(f'{key}: '))
         try:
             validate_code_input(value)
